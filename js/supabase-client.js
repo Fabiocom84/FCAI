0f833b9e1f01bf559b5cc86889a69da84e18858b
Fabiocom84 <fabiocomazzetto@gmail.com>
2025-10-04 22:30:27 +0200

    magic

--- js/supabase-client.js
+++ js/supabase-client.js
@@ -1,6 +1,6 @@
 // js/supabase-client.js
 
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.39.8/+esm';
 
 const supabaseUrl = 'https://mqfhsiezsorpdnskcsgw.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1xZmhzaWV6c29ycGRuc2tjc2d3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc1MjY3NTgsImV4cCI6MjA3MzEwMjc1OH0.JImF22KAyhAVDdVTvDWF3nwv2CyQIt8aiT76IghSFqI';
