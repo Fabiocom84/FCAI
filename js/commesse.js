847cdfbfafc1919abdad8ebb8312c368cffafa28
Fabiocom84 <fabiocomazzetto@gmail.com>
2025-12-21 15:37:58 +0100

    utente

--- js/commesse.js
+++ js/commesse.js
@@ -323,6 +323,7 @@ const App = {
         this.state.sortOrder = sortOrder;
         this.fetchCommesse(true);
     },
+    
 
     handleScroll: function() {
         if (this.state.isLoading) return;
