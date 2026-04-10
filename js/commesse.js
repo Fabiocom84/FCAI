deb5c1d7ccb8d68505ff4fba4d768abc9b84d585
Fabiocom84 <fabiocomazzetto@gmail.com>
2026-04-10 20:35:18 +0200

    mark

--- js/commesse.js
+++ js/commesse.js
@@ -1186,6 +1186,14 @@ const App = {
 let map = null;
 let currentMarker = null;
 
+// FIX: Leaflet default marker icons path (local CSS missing images/)
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+    iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+    shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+});
+
 function setupGeocodingControls() {
     const btnCalc = document.getElementById('btn-calc-geo');
     const btnMap = document.getElementById('btn-open-map');
